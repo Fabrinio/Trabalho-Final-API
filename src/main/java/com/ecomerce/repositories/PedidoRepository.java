9dc27b2dc2b2a625aefcb524f06df41650febe51
Michaelfernan <fernandes1michael@gmail.com>
2022-10-24 21:25:47 -0300

    Implementação Repository

--- src/main/java/com/ecomerce/repositories/PedidoRepository.java
+++ src/main/java/com/ecomerce/repositories/PedidoRepository.java
@@ -1,9 +1,10 @@
 package com.ecomerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ecomerce.model.Pedido;
-
+@Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
 
 }
--- src/main/java/com/ecomerce/repositories/ProdutoRepository.java
+++ src/main/java/com/ecomerce/repositories/ProdutoRepository.java
@@ -1,5 +1,11 @@
 package com.ecomerce.repositories;
 
-public interface ProdutoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecomerce.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 
 }
