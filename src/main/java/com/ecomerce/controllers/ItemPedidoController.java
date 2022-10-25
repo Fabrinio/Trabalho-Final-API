90aa304511422cc934b8f789bd9811fee954125a
Fabrinio <fabriniolucas1412@gmail.com>
2022-10-24 22:03:55 -0300

    implementação de pedido e item pedido

--- src/main/java/com/ecomerce/controllers/ItemPedidoController.java
+++ src/main/java/com/ecomerce/controllers/ItemPedidoController.java
@@ -1,5 +1,57 @@
 package com.ecomerce.controllers;
 
-public class ItemPedidoController {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecomerce.model.ItemPedido;
+import com.ecomerce.service.ItemPedidoService;
+
+@RestController 
+@RequestMapping("/api/itempedido")
+public class ItemPedidoController {
+	@Autowired
+	private ItemPedidoService servico;
+
+	@GetMapping("/buscar")
+	public ResponseEntity<List<ItemPedido>> obterTodos() {
+
+		List<ItemPedido> lista = servico.obterTodos();
+		return ResponseEntity.ok(lista); // 200
+	}
+
+	@GetMapping("/buscar/{id}")
+	public ResponseEntity<ItemPedido> obterPorId(@PathVariable Long id) {
+
+		Optional<ItemPedido> optCasa = servico.obterPorId(id);
+		return ResponseEntity.ok(optCasa.get()); // 200
+	}
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<ItemPedido> cadastrar(@RequestBody ItemPedido itemItemPedido) {
+		itemItemPedido = servico.cadastrar(itemItemPedido);
+		return new ResponseEntity<>(itemItemPedido, HttpStatus.CREATED); // 201
+	}
+
+	@PutMapping("/atualizar/{id}")
+	public ResponseEntity<ItemPedido> atualizar(@PathVariable Long id, @RequestBody ItemPedido itemItemPedido) {
+		return ResponseEntity.ok(servico.atualizar(id, itemItemPedido)); // 200
+	}
+
+	@DeleteMapping("/deletar/{id}")
+	public ResponseEntity<?> deletar(@PathVariable Long id) {
+		servico.deletar(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT); // 204
+	}
 }
